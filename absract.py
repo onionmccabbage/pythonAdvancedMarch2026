f39812806d278ea27d3de7011c13be2b2fda5b5f
onionmccabbage <onionmccabbage@live.com>
2026-03-23 10:11:44 +0000

    sample abstract class

---
+++ absract.py
@@ -0,0 +1,11 @@
+# Abstract Classes can be useful
+
+class AbstractShape():
+    '''Here we declare methods and properties that need to be included in
+    classes derived from this abtract class'''
+    def num_sides(self):
+        pass # we do not write any implementation
+    def colour(self):
+        pass
+
+
