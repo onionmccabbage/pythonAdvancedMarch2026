65281fdf43c7140c18a695c96a6a37796c7216bb
onionmccabbage <onionmccabbage@live.com>
2026-03-23 16:08:21 +0000

    using filter

---
+++ functional_prog/using_map_filter.py
@@ -0,0 +1,20 @@
+# functional programming can include higher-oder functions, 
+# where we use one function to achieve the outcome of another
+# This is especially useful with 'map' and 'filter'
+
+def isOdd(n=2):
+    '''Return True for odd values of n and False in all other cases'''
+    if n/2 == n//2:
+        return False # not odd
+    else:
+        return True  
+    
+if __name__ == '__main__':
+    print( isOdd(3) ) # False
+    # we may have a collection of values and we need just the odd members
+    d = (4,3,7,2,-7,99,1234,46,73,24)
+    # Python has a filter method for collections
+    odds = filter(isOdd, d)
+    print( odds ) # we havea filter object
+    odd_nums = [i for i in odds] # this is a list comprehension
+    print( odd_nums )
