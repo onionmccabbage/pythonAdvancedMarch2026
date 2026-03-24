bca6be4a1946b9b7124d79739c54383696c3c61c
onionmccabbage <onionmccabbage@live.com>
2026-03-24 10:18:23 +0000

    request data with parameters

--- using_api.py
+++ using_api.py
@@ -5,26 +5,34 @@ import requests
 class APIAccess():
     '''This class will expose methods to access remote API data'''
     def __init__(self):
-        pass
+        # make properties available throughout the class
+        self.apiURL = 'https://jsonplaceholder.typicode.com'
     def getAllData(self):
-        apiURL = 'https://jsonplaceholder.typicode.com'
         try:
             # request all the photos as JSON
             # NB this is asynchronous, but the requsts library will wait
             # this is an example of I/O bound code
-            response = requests.get(f'{apiURL}/photos')
+            response = requests.get(f'{self.apiURL}/photos')
             # grab the data from the response object
             data = response.json() # or .raw() etc.
-            # NB by default Python will .......
+            # NB by default Python will convert plain JSON to a structure (list, dict etc)
             return data
         except Exception as err:
             print(err)
-    def getOneData(self):
-        pass
-
+    def getOneData(self, n=1): # provide a sensible default
+        try:
+            response = requests.get(f'{self.apiURL}/photos/{n}')
+            photo = response.json()
+            return photo
+        except Exception as err:
+            print(err)
 
 if __name__ == '__main__':
     # make instances of our class and use them to grab API data
     a = APIAccess()
     d = a.getAllData()
     print(d, type(d))
+    print(d[3], type(d[3]))
+    # grab one photo
+    p = a.getOneData(34)
+    print(p, type(p))
