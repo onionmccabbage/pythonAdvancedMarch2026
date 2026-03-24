f6c4f91b8de664e3cfe83ff9243e0c77ad6757b6
onionmccabbage <onionmccabbage@live.com>
2026-03-24 10:48:45 +0000

    retrieving text

--- fifo/read_file.py
+++ fifo/read_file.py
@@ -4,9 +4,11 @@
 def readText():
     '''retrieve text from a file'''
     try:
-        with open() as fin:
-            pass
-
+        # we wish to read text
+        with open('my_file.txt', 'rt') as fin:
+            t = fin.read() # retrieve the entire contents of the file
+            # t = fin.readlines() # retrieve as a list of lines
+        return t
     except Exception as err:
         print(err)
 
@@ -15,3 +17,5 @@ def readBytes():
 
 if __name__ == '__main__':
     '''exercise this module'''
+    r = readText()
+    print(r) # the entire file contents
