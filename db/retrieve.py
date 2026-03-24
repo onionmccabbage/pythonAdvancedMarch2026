4910ee0f9554c3935d5d4cfff787251ac3230ab8
onionmccabbage <onionmccabbage@live.com>
2026-03-24 11:44:58 +0000

    retrieve from db

---
+++ db/retrieve.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def retrieve():
+    '''retrieve from the db table'''
+    # make a connection
+    conn = sqlite3.connect('my_db')
+    # get a cursor
+    curs = conn.cursor()
+    # write an SQL statement
+    st = 'SELECT creature, cost, count FROM zoo'
+    # execute and commit the SQL    
+    try:
+        curs.execute(st)
+        conn.commit()
+        # return the retrieved values
+        rows = curs.fetchall() # retrieves zero or more, up to all the rows in the table
+        return rows
+
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    a = retrieve()
+    print(a)
