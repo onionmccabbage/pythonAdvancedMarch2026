bebf3ef40e0ce6e1e2916401912a8763a88de1b7
onionmccabbage <onionmccabbage@live.com>
2026-03-25 10:16:33 +0000

    redirecting stdout

--- redirection/std_redirect.py
+++ redirection/std_redirect.py
@@ -11,9 +11,17 @@ class RedirectOut():
     # there are special methods available to classes:
     def __enter__(self): # run every time we invoke an instance
         '''swap the original stream for our new stream'''
+        self.old_stream = sys.stdout
+        sys.stdout = self.new_stream # here we replace the stdout
     # the three arguments here are used by exception handling within Python
     def __exit__(self, exc_type, exc, tb): # runs every time an invoked instance concludes
         '''swap the original stream back in'''
+        sys.stdout = self.old_stream # replace the original stream
         
 if __name__ == '__main__':
-    r = RedirectOut(True) # at this point an instance is created, so __init__ runs once
+    #  streams default to 't' for text
+    with open('my_file.txt', 'a') as fout:
+        r = RedirectOut(fout) # at this point an instance is created, so __init__ runs once
+        with r: # this invokes __enter__
+            print('this text will be redirected to the file')
+    print('normal service resumed')
---
+++ redirection/what_is_stdout.py
@@ -0,0 +1,6 @@
+import sys
+
+print (sys.stdout)
+print (sys.stdin)
+print (sys.stderr)
+print (sys.stdlib_module_names)
