ae36cd0166579e13f4066db1afe685b0bf2fc642
onionmccabbage <onionmccabbage@live.com>
2026-03-26 11:25:27 +0000

    using locks

---
+++ multithread/d_thread.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from threading import Lock
+
+# here are sme global assets
+count = 0
+lock = Lock()
+
+def fnA():
+    '''increment the count to 100'''
+    global count
+    while count <100:
+        count += 1
+        print(f'A increments to {count}') 
+
+def fnB():
+    '''decremnt the count to -100'''
+    count
+    while count >-100:
+        count -= 1
+        print(f'B decrements to {count}') 
+
+if __name__ == '__main__':
+    tA = Thread(target=fnA)
+    tB = Thread(target=fnB)
+    tA.start()
+    tB.start()
+    tA.join()
+    tB.join()
