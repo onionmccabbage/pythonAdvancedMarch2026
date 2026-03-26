cafbc9912f22de8405065951213de1d375cfad38
onionmccabbage <onionmccabbage@live.com>
2026-03-26 09:59:58 +0000

    multi threading

--- multithread/a_thread.py
+++ multithread/a_thread.py
@@ -12,8 +12,18 @@ def fn(n=1):
 if __name__ == '__main__':
     '''invoke our function on a new thread'''
     tA = Thread(target=fn, args=(1,) ) # here is a new Thread instance (a Thread of control)
-
+    tB = Thread(target=fn, args=(2,) ) # here is a new Thread instance (a Thread of control)
+    tC = Thread(target=fn, args=(3,) ) # here is a new Thread instance (a Thread of control)
+    tD = Thread(target=fn, args=(4,) ) # here is a new Thread instance (a Thread of control)
 
     # we may start our additional threads
+    tC.start()
     tA.start()
+    tB.start()
+    tD.start()
+    # remember, in normal operation the function must run consecutively
+    # fn(9) # invoke the function on the main thread
+    # fn(8)
+    # fn(7)
+    # fn(6)
 
